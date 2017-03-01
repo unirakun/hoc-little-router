1099387192eee410b340b8a477ad9aa6ea1b3906
Fabien JUIF <fabien.juif@zenika.com>
2017-03-01 12:12:06 +0100

    :tada: first commit

---
+++ src/router.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+export default (title, options) => {
+  const isRouteFound = result => result && result.title === title
+  // This is genrated prop name to avoid overlaping props given from parent
+  const propName = `show_${title}`
+
+  const mapStateToProps = (state) => {
+    if (!state.router || !state.router.result) {
+      // eslint-disable-next-line no-console
+      console.error('/HOC/ Router | There is no route found in `state.router.result`')
+      return {}
+    }
+
+    // Absolute mode, we are looking in top level only
+    let result = state.router.result
+    if (options && options.absolute) {
+      return {
+        [propName]: result.title === title,
+      }
+    }
+
+    // Either way we are looking top down the result tree
+    let found = isRouteFound(result)
+    while (result && !found) {
+      result = result.parent
+      found = isRouteFound(result)
+    }
+
+    return {
+      [propName]: !!found,
+    }
+  }
+
+  return Component => connect(mapStateToProps)(
+    (props) => {
+      if (!props[propName]) return null
+
+      // Like omit
+      // We delete this propse so the child don't see it
+      const newProps = { ...props }
+      delete newProps[propName]
+
+      return <Component {...newProps} />
+    },
+  )
+}
---
+++ src/router.spec.js
@@ -0,0 +1,66 @@
+/* eslint-disable
+ import/no-extraneous-dependencies,
+ react/jsx-filename-extension,
+ react/prop-types
+ */
+/* eslint-env jest */
+import React from 'react'
+import renderer from 'react-test-renderer'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import sinon from 'sinon'
+import router from './router'
+
+const Component = props => <div>{JSON.stringify(props)}</div>
+
+const store = createStore(() => ({
+  router: {
+    result: {
+      title: 'TITLE_1',
+      parent: {
+        title: 'TITLE_2',
+      },
+    },
+  },
+}))
+
+const snap = (title, options, st = store) => {
+  const Decorated = router(title, options)(Component)
+
+  const component = renderer.create(
+    <Provider store={st}>
+      <Decorated prop1="prop1" show />
+    </Provider>,
+  )
+
+  const tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+}
+
+describe('Router -redux-little-router- HOC', () => {
+  it('should print Component on first level', () => snap('TITLE_1'))
+  it('should not print Component', () => snap('TITLE_42'))
+  it('should print Component on second level', () => snap('TITLE_2'))
+  it('should not print Component on second level (absolute mode)', () => snap('TITLE_2', { absolute: true }))
+
+  describe('Errors', () => {
+    let spy
+    beforeEach(() => { spy = sinon.stub(console, 'error') })
+    afterEach(() => { spy.restore() })
+
+    const snapError = (state) => {
+      snap('TITLE_1', undefined, createStore(() => state))
+      expect(spy.calledOnce).toBe(true)
+      expect(spy.getCall(0).args[0]).toEqual('/HOC/ Router | There is no route found in `state.router.result`')
+    }
+
+    it('should print an error when router reducer is not found', () => snapError({}))
+    it('should print an error when no result is found', () => snapError({ router: {} }))
+  })
+})
+
+/* eslint-enable
+ import/no-extraneous-dependencies,
+ react/jsx-filename-extension,
+ react/prop-types
+ */
