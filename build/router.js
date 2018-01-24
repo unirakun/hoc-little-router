59cc1d252d2c94ce8ddc0ac41f7d4ddd59fa0c36
Fabien JUIF <fabien.juif@zenika.com>
2018-01-24 15:45:22 +0100

    :sparkles: dont use connect from react-redux

--- build/router.js
+++ build/router.js
@@ -6,58 +6,114 @@ Object.defineProperty(exports, "__esModule", {
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactRedux = require('react-redux');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var hoc = function hoc(title, options) {
-  var isRouteFound = function isRouteFound(result) {
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var getDisplayName = function getDisplayName(Component) {
+  return 'router(' + (Component.displayName || Component.name || Component.constructor && Component.constructor.name || 'Unknown') + ')';
+};
+
+var isRouteFound = function isRouteFound(title) {
+  return function (result) {
     return result && [].concat(title).includes(result.title);
   };
+};
 
-  // This is genrated prop name to avoid overlaping props given from parent
-  var propName = 'show_' + title;
-
-  var mapStateToProps = function mapStateToProps(state) {
-    if (!state.router || !state.router.result) {
-      // eslint-disable-next-line no-console
-      console.error('/HOC/ Router | There is no route found in `state.router.result`');
-      return {};
-    }
-
-    // Absolute mode, we are looking in top level only
-    var result = state.router.result;
-    if (options && options.absolute) {
-      return _defineProperty({}, propName, isRouteFound(result));
-    }
-
-    // Either way we are looking top down the result tree
-    var found = isRouteFound(result);
-    while (result && !found) {
-      result = result.parent;
-      found = isRouteFound(result);
-    }
-
-    return _defineProperty({}, propName, !!found);
-  };
-
+var hoc = function hoc(title, options) {
   return function (Component) {
-    return (0, _reactRedux.connect)(mapStateToProps)(function (props) {
-      if (!props[propName]) return null;
+    var _class, _temp;
 
-      // Like omit
-      // We delete this propse so the child don't see it
-      var newProps = _extends({}, props);
-      delete newProps[propName];
+    return _temp = _class = function (_React$Component) {
+      _inherits(_class, _React$Component);
 
-      return _react2.default.createElement(Component, newProps);
-    });
+      function _class(props, context) {
+        _classCallCheck(this, _class);
+
+        var _this = _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).call(this, props, context));
+
+        _this.toShow = function () {
+          var state = _this.context.store.getState();
+
+          if (!state.router || !state.router.result) {
+            // eslint-disable-next-line no-console
+            console.error('/HOC/ Router | There is no route found in `state.router.result`');
+            return;
+          }
+
+          // Absolute mode, we are looking in top level only
+          var result = state.router.result;
+          if (options && options.absolute) {
+            var _show = isRouteFound(title)(result);
+            if (_show !== _this.state.show) {
+              _this.setState(function (innerState) {
+                return _extends({}, innerState, { show: _show });
+              });
+            }
+
+            return;
+          }
+
+          // Either way we are looking top down the result tree
+          var show = isRouteFound(title)(result);
+          while (result && !show) {
+            result = result.parent;
+            show = isRouteFound(title)(result);
+          }
+
+          if (show !== _this.state.show) {
+            _this.setState(function (innerState) {
+              return _extends({}, innerState, { show: show });
+            });
+          }
+        };
+
+        _this.state = { show: false };
+        return _this;
+      }
+
+      _createClass(_class, [{
+        key: 'componentWillMount',
+        value: function componentWillMount() {
+          var _this2 = this;
+
+          // subscribe
+          this.unsubscribe = this.context.store.subscribe(function () {
+            _this2.toShow();
+          });
+
+          // run in once
+          this.toShow();
+        }
+      }, {
+        key: 'componentWillUnmount',
+        value: function componentWillUnmount() {
+          this.unsubscribe();
+        }
+      }, {
+        key: 'render',
+        value: function render() {
+          if (!this.state.show) return null;
+
+          return _react2.default.createElement(Component, this.props);
+        }
+      }]);
+
+      return _class;
+    }(_react2.default.Component), _class.displayName = getDisplayName(Component), _class.contextTypes = {
+      store: function store() {
+        return null;
+      } }, _temp;
   };
 };
 
--- src/router.jsx
+++ src/router.jsx
@@ -1,51 +1,78 @@
 import React from 'react'
-import { connect } from 'react-redux'
 
-const hoc = (title, options) => {
-  const isRouteFound = result => result && [].concat(title).includes(result.title)
+const getDisplayName = Component => `router(${
+  Component.displayName
+  || Component.name
+  || (Component.constructor && Component.constructor.name)
+  || 'Unknown'
+})`
 
-  // This is genrated prop name to avoid overlaping props given from parent
-  const propName = `show_${title}`
+const isRouteFound = title => result => result && [].concat(title).includes(result.title)
 
-  const mapStateToProps = (state) => {
+const hoc = (title, options) => Component => class extends React.Component {
+  static displayName = getDisplayName(Component)
+
+  static contextTypes = {
+    store: () => null, // this is to avoid importing prop-types
+  }
+
+  constructor(props, context) {
+    super(props, context)
+
+    this.state = { show: false }
+  }
+
+  componentWillMount() {
+    // subscribe
+    this.unsubscribe = this.context.store.subscribe(() => {
+      this.toShow()
+    })
+
+    // run in once
+    this.toShow()
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe()
+  }
+
+  toShow = () => {
+    const state = this.context.store.getState()
+
     if (!state.router || !state.router.result) {
       // eslint-disable-next-line no-console
       console.error('/HOC/ Router | There is no route found in `state.router.result`')
-      return {}
+      return
     }
 
     // Absolute mode, we are looking in top level only
     let result = state.router.result
     if (options && options.absolute) {
-      return {
-        [propName]: isRouteFound(result),
+      const show = isRouteFound(title)(result)
+      if (show !== this.state.show) {
+        this.setState(innerState => ({ ...innerState, show }))
       }
+
+      return
     }
 
     // Either way we are looking top down the result tree
-    let found = isRouteFound(result)
-    while (result && !found) {
+    let show = isRouteFound(title)(result)
+    while (result && !show) {
       result = result.parent
-      found = isRouteFound(result)
+      show = isRouteFound(title)(result)
     }
 
-    return {
-      [propName]: !!found,
+    if (show !== this.state.show) {
+      this.setState(innerState => ({ ...innerState, show }))
     }
   }
 
-  return Component => connect(mapStateToProps)(
-    (props) => {
-      if (!props[propName]) return null
+  render() {
+    if (!this.state.show) return null
 
-      // Like omit
-      // We delete this propse so the child don't see it
-      const newProps = { ...props }
-      delete newProps[propName]
-
-      return <Component {...newProps} />
-    },
-  )
+    return <Component {...this.props} />
+  }
 }
 
 hoc.absolute = (title, options) => hoc(title, { ...options, absolute: true })
